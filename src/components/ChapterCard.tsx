11d7353f7d7fddc9ac8956da7cfb9dbaa7b71c55
bhav-07 <jangirbhavani201@gmail.com>
2023-09-09 19:43:32 +0530

    added handler

--- src/components/ChapterCard.tsx
+++ src/components/ChapterCard.tsx
@@ -6,17 +6,29 @@ import axios from 'axios'
 import React from 'react'
 
 type Props = { chapter: Chapter, chapterIndex: number }
+export type ChapterCardHandler = {
+  triggerLoad: () => void
+}
 
-const ChapterCard = ({ chapter, chapterIndex }: Props) => {
+const ChapterCard = React.forwardRef<ChapterCardHandler,Props>(({chapter,chapterIndex},ref) => {
   const [success, setSucess] = React.useState<boolean | null>(null)
   const { mutate: getChapterInfo, isLoading } = useMutation(
     {
       mutationFn: async () => {
-        const response = await axios.post('/api/chapter/getInfo')
+        const response = await axios.post('/api/chapter/getInfo',{chapterId: chapter.id})
         return response.data
       }
     }
-  )
+    )
+    React.useImperativeHandle(ref,()=>({
+      async triggerLoad() {
+        getChapterInfo(undefined,{
+          onSuccess: () => {
+            console.log('success')
+          }
+        })
+      }
+    }))
   return (
     <div key={chapter.id} className={cn('px-4 py-2 mt-2 rounded flex justify-between',
       {
@@ -24,9 +36,11 @@ const ChapterCard = ({ chapter, chapterIndex }: Props) => {
         'bg-red-400': success === false,
         'bg-green-500': success === true
       })}>
-      <h5>Chapter {chapterIndex + 1} : {chapter.name}</h5>
+     <h5>{chapter.name}</h5>
     </div>
   )
-}
+})
+
+ChapterCard.displayName = 'ChapterCard'
 
 export default ChapterCard
