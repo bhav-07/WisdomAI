a5f363297e0766045d8baec1a7e8911cdd54bf86
bhav-07 <jangirbhavani201@gmail.com>
2023-11-06 20:55:52 +0530

    Refactor ChapterCard component and add completedChapters functionality.

--- src/components/ChapterCard.tsx
+++ src/components/ChapterCard.tsx
@@ -1,46 +1,90 @@
-'use client'
-import { cn } from '@/lib/utils'
-import { Chapter } from '@prisma/client'
-import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
-import React from 'react'
+"use client";
+import { cn } from "@/lib/utils";
+import { Chapter } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { useToast } from "./ui/use-toast";
+import { Loader2 } from "lucide-react";
 
-type Props = { chapter: Chapter, chapterIndex: number }
+type Props = {
+  chapter: Chapter;
+  chapterIndex: number;
+  completedChapters: Set<String>;
+  setCompletedChapters: React.Dispatch<React.SetStateAction<Set<String>>>;
+};
 export type ChapterCardHandler = {
-  triggerLoad: () => void
-}
+  triggerLoad: () => void;
+};
 
-const ChapterCard = React.forwardRef<ChapterCardHandler,Props>(({chapter,chapterIndex},ref) => {
-  const [success, setSucess] = React.useState<boolean | null>(null)
-  const { mutate: getChapterInfo, isLoading } = useMutation(
-    {
+const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(
+  ({ chapter, chapterIndex, setCompletedChapters, completedChapters }, ref) => {
+    const { toast } = useToast();
+    const [success, setSucess] = React.useState<boolean | null>(null);
+    const { mutate: getChapterInfo, isLoading } = useMutation({
       mutationFn: async () => {
-        const response = await axios.post('/api/chapter/getInfo',{chapterId: chapter.id})
-        return response.data
+        const response = await axios.post("/api/chapter/getInfo", {
+          chapterId: chapter.id,
+        });
+        return response.data;
+      },
+    });
+
+    const addChapterIdToSet = React.useCallback(() => {
+      setCompletedChapters((prev) => {
+        const newSet = new Set(prev);
+        newSet.add(chapter.id);
+        return newSet;
+      });
+    }, [chapter.id, setCompletedChapters]);
+
+    React.useEffect(() => {
+      if (chapter.videoId) {
+        setSucess(true);
+        addChapterIdToSet();
       }
-    }
-    )
-    React.useImperativeHandle(ref,()=>({
+    }, [chapter, addChapterIdToSet]);
+
+    React.useImperativeHandle(ref, () => ({
       async triggerLoad() {
-        getChapterInfo(undefined,{
+        if (chapter.videoId) {
+          addChapterIdToSet();
+          return;
+        }
+        getChapterInfo(undefined, {
           onSuccess: () => {
-            console.log('success')
-          }
-        })
-      }
-    }))
-  return (
-    <div key={chapter.id} className={cn('px-4 py-2 mt-2 rounded flex justify-between',
-      {
-        'bg-secondary': success === null,
-        'bg-red-400': success === false,
-        'bg-green-500': success === true
-      })}>
-     <h5>{chapter.name}</h5>
-    </div>
-  )
-})
+            setSucess(true);
+            addChapterIdToSet();
+          },
+          onError: (error) => {
+            console.error(error);
+            setSucess(false);
+            toast({
+              title: "Error",
+              description: "There was an error loading your chapter.",
+              variant: "destructive",
+            });
+            addChapterIdToSet();
+          },
+        });
+      },
+    }));
+    return (
+      <div
+        key={chapter.id}
+        className={cn("px-4 py-2 mt-2 rounded flex justify-between", {
+          "bg-secondary": success === null,
+          "bg-red-400": success === false,
+          "bg-green-500": success === true,
+        })}
+      >
+        <h5>{chapter.name}</h5>
+        {isLoading && <Loader2 className="animate-spin" />}
+      </div>
+    );
+  }
+);
 
-ChapterCard.displayName = 'ChapterCard'
+ChapterCard.displayName = "ChapterCard";
 
-export default ChapterCard
+export default ChapterCard;
