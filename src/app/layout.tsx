8f2e56c94a7bb4e51d6dc88fd781b97d19364a7e
bhav-07 <jangirbhavani201@gmail.com>
2023-11-06 20:55:14 +0530

    Add Toaster and Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Lexend } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import { Provider } from '@/components/Providers'
+import { Toaster } from '@/components/ui/toaster'
 
-const lexend = Lexend({subsets: ['latin']})
+const lexend = Lexend({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'WisdomAI',
@@ -18,9 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(lexend.className,'antialiased min-h-screen pt-16')}>
-        <Navbar />  
-        {children}</body>
+      <body className={cn(lexend.className, 'antialiased min-h-screen pt-16')}>
+        <Provider>
+          <Navbar />
+          {children}
+          <Toaster/>
+        </Provider>
+      </body>
     </html>
   )
 }
